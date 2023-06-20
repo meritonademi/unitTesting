fce7a9e3fc8898322914b58bb8c372dd5db8742d
Meriton Ademi <ma28727@seeu.edu.mk>
2023-06-20 23:15:25 +0200

    initial

---
+++ SOAProjectTests/EmployeeRepositoryTest.cs
@@ -0,0 +1,12 @@
+using Moq;
+using SOAProject.Data;
+using SOAProject.DTOs;
+using SOAProject.Models;
+using SOAProject.Repositories.EmployeeRepository;
+
+namespace SOAProjectTests;
+
+public class EmployeeRepositoryTest
+{
+ 
+}
---
+++ SOAProjectTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace SOAProjectTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
