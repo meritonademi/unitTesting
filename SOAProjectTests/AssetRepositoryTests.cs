ee9fac38aea6c69e6f243cf2d632060df4e5d13f
Meriton Ademi <ma28727@seeu.edu.mk>
2023-06-25 15:56:58 +0200

    asdasd

---
+++ SOAProjectTests/AssetRepositoryTests.cs
@@ -0,0 +1,122 @@
+using Microsoft.EntityFrameworkCore;
+using SOAProject.Data;
+using SOAProject.Models;
+using SOAProject.Repositories.AssetRepository;
+
+namespace SOAProject.Tests
+{
+    public class AssetRepositoryTests
+    {
+        private readonly DbContextOptions<AppDbContext> _options;
+
+        public AssetRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+        }
+
+        [Fact]
+        public async Task GetAllAssets_ShouldReturnAllAssetsWithCategories()
+        {
+            using (var context = new AppDbContext(_options))
+            {
+                var assetRepository = new AssetRepository(context);
+                await SeedDatabase(context);
+                var assets = await assetRepository.GetAllAssets();
+                
+                Assert.NotNull(assets);
+                Assert.Equal(3, assets.Count());
+                foreach (var asset in assets)
+                {
+                    Assert.NotNull(asset.Category);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task GetAssetById_ExistingId_ShouldReturnAssetWithCategory()
+        {
+            using (var context = new AppDbContext(_options))
+            {
+                var assetRepository = new AssetRepository(context);
+                await SeedDatabase(context); 
+                var asset = await assetRepository.GetAssetById(1);
+
+                Assert.NotNull(asset);
+                Assert.Equal(1, asset.Id);
+                Assert.NotNull(asset.Category);
+            }
+        }
+
+        [Fact]
+        public async Task GetAssetById_NonExistingId_ShouldReturnNull()
+        {
+            using (var context = new AppDbContext(_options))
+            {
+                var assetRepository = new AssetRepository(context);
+                await SeedDatabase(context); 
+                var asset = await assetRepository.GetAssetById(99);
+                Assert.Null(asset);
+            }
+        }
+
+        [Fact]
+        public void AddAssetAsync_ShouldAddAssetToDatabase()
+        {
+            using (var context = new AppDbContext(_options))
+            {
+                var assetRepository = new AssetRepository(context);
+                var asset = new Asset { Id = 4, Name = "New Asset", CategoryId = 1 };
+
+                // Act
+                var addedAsset = assetRepository.AddAssetAsync(asset);
+
+                // Assert
+                Assert.Equal(4, addedAsset.Id);
+                Assert.Contains(context.Assets, a => a.Id == 4);
+            }
+        }
+
+        [Fact]
+        public async Task UpdateAsset_ShouldUpdateAssetInDatabase()
+        {
+            using (var context = new AppDbContext(_options))
+            {
+                var assetRepository = new AssetRepository(context);
+                await SeedDatabase(context); 
+                var assetToUpdate = new Asset { Id = 1, Name = "Updated Asset", CategoryId = 2 };
+
+                var asset = await assetRepository.GetAssetById(1);
+                assetRepository.UpdateAsset(asset, assetToUpdate);
+
+                Assert.Equal("Updated Asset", asset.Name);
+                Assert.Equal(2, asset.CategoryId);
+            }
+        }
+
+        [Fact]
+        public void RemoveAsset_ShouldRemoveAssetFromDatabase()
+        {
+            using (var context = new AppDbContext(_options))
+            {
+                var assetRepository = new AssetRepository(context);
+                var assetToRemove = new Asset { Id = 1, Name = "Asset 1", CategoryId = 1 };
+                assetRepository.RemoveAsset(assetToRemove);
+                Assert.DoesNotContain(context.Assets, a => a.Id == 1);
+            }
+        }
+
+        private async Task SeedDatabase(AppDbContext context)
+        {
+            var category1 = new Category { Id = 1, Name = "Category 1" };
+            var category2 = new Category { Id = 2, Name = "Category 2" };
+            var asset1 = new Asset { Id = 1, Name = "Asset 1", CategoryId = 1 };
+            var asset2 = new Asset { Id = 2, Name = "Asset 2", CategoryId = 2 };
+            var asset3 = new Asset { Id = 3, Name = "Asset 3", CategoryId = 1 };
+
+            context.Categories.AddRange(category1, category2);
+            context.Assets.AddRange(asset1, asset2, asset3);
+            await context.SaveChangesAsync();
+        }
+    }
+}
--- SOAProjectTests/EmployeeRepositoryTest.cs
+++ SOAProjectTests/EmployeeRepositoryTest.cs
@@ -1,4 +1,4 @@
-using Moq;
+using Microsoft.EntityFrameworkCore;
 using SOAProject.Data;
 using SOAProject.DTOs;
 using SOAProject.Models;
@@ -8,5 +8,121 @@ namespace SOAProjectTests;
 
 public class EmployeeRepositoryTest
 {
- 
+    private readonly DbContextOptions<AppDbContext> _options;
+
+    public EmployeeRepositoryTest()
+    {
+        _options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "test_database")
+            .Options;
+    }
+
+    [Fact]
+    public async Task GetAllEmployees_ShouldReturnAllEmployeesWithDepartments()
+    {
+        // Arrange
+        using (var context = new AppDbContext(_options))
+        {
+            var employeeRepository = new EmployeeRepository(context);
+            await SeedDatabase(context);
+            var employees = await employeeRepository.GetAllEmployees();
+
+            Assert.NotNull(employees);
+            Assert.Equal(3, employees.Count());
+            foreach (var employee in employees)
+            {
+                Assert.NotNull(employee.Department);
+            }
+        }
+    }
+
+    [Fact]
+    public async Task GetEmployeeById_ExistingId_ShouldReturnEmployeeWithDepartment()
+    {
+        using (var context = new AppDbContext(_options))
+        {
+            var employeeRepository = new EmployeeRepository(context);
+            await SeedDatabase(context);
+            var employee = await employeeRepository.GetEmployeeById(1);
+            Assert.NotNull(employee);
+            Assert.Equal(1, employee.Id);
+            Assert.NotNull(employee.Department);
+        }
+    }
+
+    [Fact]
+    public async Task GetEmployeeById_NonExistingId_ShouldReturnNull()
+    {
+        using (var context = new AppDbContext(_options))
+        {
+            var employeeRepository = new EmployeeRepository(context);
+            await SeedDatabase(context);
+            var employee = await employeeRepository.GetEmployeeById(99);
+
+            Assert.Null(employee);
+        }
+    }
+
+    [Fact]
+    public void AddEmployeeAsync_ShouldAddEmployeeToDatabase()
+    {
+        using (var context = new AppDbContext(_options))
+        {
+            var employeeRepository = new EmployeeRepository(context);
+            var employee = new Employee
+                { Id = 4, Name = "John", SurName = "Doe", DepartmentId = 1, Tel = "1234567890" };
+
+            var addedEmployee = employeeRepository.AddEmployeeAsync(employee);
+
+            Assert.Equal(4, addedEmployee.Id);
+            Assert.Contains(context.Employees, e => e.Id == 4);
+        }
+    }
+
+    [Fact]
+    public async Task UpdateEmployee_ShouldUpdateEmployeeInDatabase()
+    {
+        using (var context = new AppDbContext(_options))
+        {
+            var employeeRepository = new EmployeeRepository(context);
+            await SeedDatabase(context);
+            var employeeToUpdate = new EmployeeDTO
+                { Name = "Updated", SurName = "Employee", DepartmentId = 2, Tel = "9876543210" };
+
+            var employee = await employeeRepository.GetEmployeeById(1);
+            employeeRepository.UpdateEmployee(employee, employeeToUpdate);
+
+            Assert.Equal("Updated", employee.Name);
+            Assert.Equal("Employee", employee.SurName);
+            Assert.Equal(2, employee.DepartmentId);
+            Assert.Equal("9876543210", employee.Tel);
+        }
+    }
+
+    [Fact]
+    public void RemoveEmployee_ShouldRemoveEmployeeFromDatabase()
+    {
+        using (var context = new AppDbContext(_options))
+        {
+            var employeeRepository = new EmployeeRepository(context);
+            var employeeToRemove = new Employee
+                { Id = 1, Name = "John", SurName = "Doe", DepartmentId = 1, Tel = "1234567890" };
+            employeeRepository.RemoveEmployee(employeeToRemove);
+            Assert.DoesNotContain(context.Employees, e => e.Id == 1);
+        }
+    }
+
+    private async Task SeedDatabase(AppDbContext context)
+    {
+        var department1 = new Department { Id = 1, Name = "Department 1" };
+        var department2 = new Department { Id = 2, Name = "Department 2" };
+        var employee1 = new Employee { Id = 1, Name = "John", SurName = "Doe", DepartmentId = 1, Tel = "1234567890" };
+        var employee2 = new Employee { Id = 2, Name = "Jane", SurName = "Smith", DepartmentId = 2, Tel = "9876543210" };
+        var employee3 = new Employee
+            { Id = 3, Name = "Alice", SurName = "Johnson", DepartmentId = 1, Tel = "5555555555" };
+
+        context.Departments.AddRange(department1, department2);
+        context.Employees.AddRange(employee1, employee2, employee3);
+        await context.SaveChangesAsync();
+    }
 }
